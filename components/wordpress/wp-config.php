96fbef6206a9406a7e12616e906b69653f6eb19d
Erik Berdonces Bonelo <sturgelose@gmail.com>
2016-05-02 17:19:51 +0200

    Integrate WP-demo in it
    Still there is a problem that the base_url of the site gets set to localhost

---
+++ components/wordpress/wp-config.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'tastamam');
+
+/** MySQL hostname */
+define('DB_HOST', 'mysql');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '2d16f9f40e290e6e02bc03aaf2d32a1b06f80aad');
+define('SECURE_AUTH_KEY',  'a5918af5387f2834ef2da78c590f98ca221240bd');
+define('LOGGED_IN_KEY',    '3c7c54e6e74b948ecafdaa82a6179a1904940451');
+define('NONCE_KEY',        '7dec586030ff4cdbf13b6e9698793473e8f06414');
+define('AUTH_SALT',        '0b1d51b95189fda06782a6453183366aba69567f');
+define('SECURE_AUTH_SALT', '85d8550ef6ce42faa4664ab642957d183052da12');
+define('LOGGED_IN_SALT',   '1b432cda147ff814a41efcd975fe1e5bcdd543b5');
+define('NONCE_SALT',       '710f1f607c3a4e8744dbef6a5994cca6f9a5d727');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ components/wordpress/wp-install.rb
@@ -0,0 +1,50 @@
+#wordpress installation script, adds the necessary config for wp and its plugins to the db
+require 'json'
+require 'fileutils'
+
+puts %x{bash /opt/wp-parent-entrypoint.sh apache2}
+#info on wp cli installation
+puts %x{wp --info --allow-root}
+
+#install wp - just add the entries in the database no wp.zip extraction
+puts 'initializing the database'
+puts %x{wp core install --allow-root}
+
+#install and activate generic-openid-connect plugin
+puts %x{wp plugin install /etc/wordpress-config/generic-openid-connect.1.0.zip --activate --allow-root --force}
+
+class FileReader
+  def read
+    file = File.open("/etc/wordpress-config/openidConfig.json", "rb")
+    #file = File.open("openidConfig.json", "rb")
+    file.read
+  end
+end
+
+fileReader = FileReader.new
+fileContents = fileReader.read
+openIdConfig = JSON.parse(fileContents)
+
+
+# Replace the base URL if we have it set as an ENV variable
+if !ENV["OIDC_URL_BASE"].nil?
+  openIdConfig["gen_openid_con_ep_login"] = openIdConfig["gen_openid_con_ep_login"].sub! 'https://federation.cyclone-project.eu', ENV["OIDC_URL_BASE"]
+  openIdConfig["gen_openid_con_ep_token"] = openIdConfig["gen_openid_con_ep_token"].sub! 'https://federation.cyclone-project.eu', ENV["OIDC_URL_BASE"]
+  openIdConfig["gen_openid_con_ep_userinfo"] = openIdConfig["gen_openid_con_ep_userinfo"].sub! 'https://federation.cyclone-project.eu', ENV["OIDC_URL_BASE"]
+end
+
+
+openIdConfig.each do |key,value|
+puts %x{wp option update #{key} '#{value}' --allow-root}
+end
+
+openIdConfig.each do |key,value|
+puts %x{wp option get #{key} --allow-root}
+end
+
+#copy .htaccess
+FileUtils.cp('/etc/wordpress-config/.htaccess', '/var/www/html')
+
+#copy pre configured wp-config.php TODO change if the parent image works now
+
+%x{apachectl -k start && tail -f /dev/null}
